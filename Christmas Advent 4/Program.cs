5d71751155b5dd99908860ebb8379784c1ab9359
Louis Braidwood <louisbraidwood@gmail.com>
2025-12-06 21:32:57 +0000

    Rewrite problem 4 to make better use of linq technologies.

--- Christmas Advent 4/Program.cs
+++ Christmas Advent 4/Program.cs
@@ -1,42 +1,59 @@
-﻿var input = File.ReadLines("res/mapGrid.csv");
+﻿using System.Linq;
+var input = File.ReadAllLines("res/mapGrid.csv");
 
-char[][] shelfSlots = input.Select(shelfRow =>
+char[,] shelfSlots = new char[input.Length, input[0].Length];
+
+for (int i = 0; i < shelfSlots.GetLength(0); i++)
 {
-    return shelfRow.ToCharArray();
-}).ToArray();
+    for (int j = 0; j < shelfSlots.GetLength(1); j++)
+    {
+        shelfSlots[i, j] = input[i][j];
+    }
+}
 
 int finalCount = 0;
-while (paperFound(out var addCount)) { finalCount += addCount; }
+while (PaperFound(out var addCount)) { finalCount += addCount; }
 
 Console.WriteLine(finalCount);
 
-bool paperFound(out int count)
+bool PaperFound(out int count)
 {
     count = 0;
-    for (int i = 0; i < shelfSlots.Length; i++)
+
+    var searchSpace = Enumerable.Range(0, shelfSlots.GetLength(0))
+           .SelectMany(i =>
+           {
+               return Enumerable.Range(0, shelfSlots.GetLength(1))
+               .Select(j => (i, j));
+           });
+    var accessibleLocations = searchSpace.Where(point =>
     {
-        for (int j = 0; j < shelfSlots[i].Length; j++)
-        {
-            if (shelfSlots[i][j] == '@' && canBeAccessed(shelfSlots, i, j))
-            {
-                shelfSlots[i][j] = '.';
-                count++;
-            }
-        }
-    }
+        return shelfSlots[point.i, point.j] == '@' && CanBeAccessed(shelfSlots, point.i, point.j);
+    });
+    List<(int, int)> locations = accessibleLocations.ToList();
+    count += locations.Count;
+    TakeMaps(locations);
     return count > 0;
 }
 
-bool canBeAccessed(char[][] shelf, int x, int y){
+void TakeMaps(List<(int, int)> map)
+{
+    foreach((int x, int y) point in map)
+    {
+        shelfSlots[point.x,point.y] = '.';
+    }
+}
+
+bool CanBeAccessed(char[,] shelf, int x, int y){
     //-1 to account for the roll of paper at (x, y)
     int count = -1;
     for(int i = 0; i < 9; i++)
     {
         int col = x + i / 3 - 1;
         int row = y + i % 3 - 1;
-        if (col < 0 || col >= shelf.Length) continue;
-        if (row < 0 || row >= shelf[0].Length) continue;
-        if (shelf[col][row] == '@') count++;
+        if (col < 0 || col >= shelf.GetLength(0)) continue;
+        if (row < 0 || row >= shelf.GetLength(1)) continue;
+        if (shelf[col, row] == '@') count++;
     }
     if (count < 4) return true;
     return false;
